19a27f931022b7faac056dbc1f77088b1d7d5148
Agourd06 <oualidagourd@gmail.com>
2025-12-27 14:12:02 +0100

    add port front

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173, // Frontend dev server port
+  },
   css: {
     postcss: './postcss.config.js',
   },
